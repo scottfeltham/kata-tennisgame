930c6ec426aabebd895fe9e160443df6a43a560b
Michael Crossman <michael.crossman@digital.hmrc.gov.uk>
2015-03-25 09:03:32 +0000

    Michael and Alex's unit tests

--- src/main/scala/com/example/tennis.scala
+++ src/main/scala/com/example/tennis.scala
@@ -2,18 +2,54 @@ package com.example
 
 class TennisGame(player1: Player, player2: Player) {
 
-  def score = {
-    "love, love"
+  def score: String = {
+
+    if(player1.scoreDescription == "forty" && player2.scoreDescription == "forty") {
+
+      if(player1.compare(player2) == -2)
+        return player2.name + " won"
+
+      if(player1.compare(player2) == 2)
+        return player1.name + " won"
+
+      if (player1.compare(player2) == 0)
+        return "deuce"
+
+      if (player1.compare(player2) == 1)
+        return "advantage " + player1.name
+
+      if (player1.compare(player2) == -1)
+        return "advantage " + player2.name
+
+    }
+
+    return player1.scoreDescription + ", " + player2.scoreDescription
   }
 
 }
 
 class Player(val name: String) extends Ordered[Player] {
 
-  def scoreDescription = {}
-  def winBall {}
+  var score: Int = 0
 
-  override def compare(that: Player): Int = ???
+  def scoreDescription: String = score match {
+      case 0 => "love"
+      case 1 => "fifteen"
+      case 2 => "thirty"
+      case _ => "forty"
+  }
+
+  def winBall: Unit = {
+    score = score + 1
+  }
+
+  override def compare(that: Player): Int = {
+    if (this.score > that.score + 1) return 2
+    if (this.score + 1 < that.score) return -2
+    if (this.score > that.score) return 1
+    if (this.score < that.score) return -1
+    return 0
+  }
 }
 
 object Player {
--- src/test/scala/TennisGameTest.scala
+++ src/test/scala/TennisGameTest.scala
@@ -15,24 +15,175 @@ class TennisGameTest extends FlatSpec with Matchers {
     tennisGame.score should be ("love, love")
   }
 
-  /*
-  it should "have score love, fifteen when player two scores" in {
-    val player1 = new Player("Mike")
-    val player2 = new Player("Roman")
-
-    val tennisGame = new TennisGame(player1, player2)
-    player2.winBall
-    tennisGame.score should be ("love, fifteen")
-  }*/
-
-  "a player" should "at the start of the game have a score of zero" in {
+  "both players" should "at the start of the game have a score of zero" in {
     val player1 = new Player("Mike")
     val player2 = new Player("Roman")
 
     val tennisGame = new TennisGame(player1, player2)
     player1.scoreDescription should be ("love")
     player2.scoreDescription should be ("love")
-
   }
+
+  "a player" should "have a score of 1 when they win their first point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.scoreDescription should be ("fifteen")
+    player2.scoreDescription should be ("love")
+  }
+
+  "A Tennis game" should "have score fifteen, love when player one scores" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    tennisGame.score should be ("fifteen, love")
+  }
+
+  "A Tennis game" should "have score thirty, love when player one scores again" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    tennisGame.score should be ("thirty, love")
+  }
+
+  "A Tennis game" should "have score forty, love when player one scores again" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    tennisGame.score should be ("forty, love")
+  }
+
+  "A Tennis game" should "have score forty, fifteen when player two scores their first point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    tennisGame.score should be ("forty, fifteen")
+  }
+
+  "A Tennis game" should "have score forty, thirty when player two scores their second point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    tennisGame.score should be ("forty, thirty")
+  }
+
+  "A Tennis game" should "have score deuce when player two scores their third point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    tennisGame.score should be ("deuce")
+  }
+
+  "A Tennis game" should "have score advantage Mike when player one scores their fourth point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    player1.winBall
+    tennisGame.score should be ("advantage Mike")
+  }
+
+  "A Tennis game" should "have score deuce when player two scores their fourth point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    player1.winBall
+    player2.winBall
+    tennisGame.score should be ("deuce")
+  }
+
+  "A Tennis game" should "have score advantage Roman when player two scores their fifth point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    tennisGame.score should be ("advantage Roman")
+  }
+
+  "A Tennis game" should "have a winner called Roman when player two scores their sixth point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    tennisGame.score should be ("Roman won")
+  }
+
+  "A Tennis game" should "have a winner called Mike when player one scores their sixth point" in {
+    val player1 = new Player("Mike")
+    val player2 = new Player("Roman")
+
+    val tennisGame = new TennisGame(player1, player2)
+    player1.winBall
+    player1.winBall
+    player1.winBall
+    player2.winBall
+    player2.winBall
+    player2.winBall
+    player1.winBall
+    player1.winBall
+    tennisGame.score should be ("Mike won")
+  }
+
 
 }
